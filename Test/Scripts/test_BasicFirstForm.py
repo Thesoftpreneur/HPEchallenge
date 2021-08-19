074ce5b44ce203294d276f04a3753a52f1ecae20
Jason <extremewinds@gmail.com>
2021-08-19 18:43:36 +0200

    add case with searchphrase: "flash"

--- Test/Scripts/test_BasicFirstForm.py
+++ Test/Scripts/test_BasicFirstForm.py
@@ -22,6 +22,18 @@ class EasySelenium_Forms(WebDriverSetup):
         assert number_of_results == 4
         assert does_all_starts_with_phrase
 
+    def test_SearchFieldResults_flashcase(self):
+        driver = self.driver
+        driver.get("https://www.hpe.com/us/en/home.html")
+        driver.set_page_load_timeout(5)
+        hpehomepage = HPEHomePage(driver)
+
+        searched_phrase = "flash"
+        hpehomepage.fillSearchFormField(searched_phrase)
+        number_of_results, does_all_starts_with_phrase = hpehomepage.printResultsInConsole(searched_phrase)
+        assert number_of_results == 3
+        assert does_all_starts_with_phrase
+
 
 if __name__ == '__main__':
     unittest.main()
